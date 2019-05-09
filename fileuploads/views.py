0527cc21de0b063791e12a3c236136dd94890649
mujeebishaque <mujeeb.ishaque@gmail.com>
2019-05-10 01:44:18 +0500

    working with views

--- fileuploads/views.py
+++ fileuploads/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 def index(request):
     
-    return render(request, 'index.html')
+
+    if request.method == 'POST':
+
+        uplaodedFile = request.FILES['file']
+        fs = FileSystemStorage()
+        fs.save(uplaodedFile.name, uplaodedFile) #returns actual uploaded file name which means if uploaded file existed and it's overwritten, it will return overwritten name
+
+    return render(request, 'index.html', context={'content': 'File Uploaded Successfully!'})
